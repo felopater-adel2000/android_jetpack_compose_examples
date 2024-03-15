702f99609f132455ea3afbf7c21b640561fc1df6
felopater-adel2000 <felopater.adel@tera.ltd>
2024-03-15 18:17:34 +0200

    Add Function to Observe View Lifeycle

--- app/src/main/java/com/restart/jetpack_compose_examples/TestScreen.kt
+++ app/src/main/java/com/restart/jetpack_compose_examples/TestScreen.kt
@@ -10,13 +10,20 @@ import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalLifecycleOwner
+import androidx.compose.ui.platform.LocalView
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleEventObserver
+import androidx.lifecycle.LifecycleObserver
+import androidx.lifecycle.LifecycleOwner
 import kotlinx.coroutines.launch
 
 
@@ -47,6 +54,38 @@ fun TestSideEffectScreen()
             }
         ) {_ ->
 
+        }
+    }
+}
+
+@Composable
+fun AttachViewsLifecycleCallback(
+    onCreate: () -> Unit,
+    onStart: () -> Unit,
+    onResume: () -> Unit,
+    onPause: () -> Unit,
+    onStop: () -> Unit,
+    onDestroy: () -> Unit,
+    viewLifecycle : LifecycleOwner = LocalLifecycleOwner.current
+)
+{
+    DisposableEffect(viewLifecycle) {
+        val lifecycleEventObserver = LifecycleEventObserver{ _, event ->
+            when(event){
+                Lifecycle.Event.ON_CREATE -> onCreate()
+                Lifecycle.Event.ON_START -> onStart()
+                Lifecycle.Event.ON_RESUME -> onResume()
+                Lifecycle.Event.ON_PAUSE -> onPause()
+                Lifecycle.Event.ON_STOP -> onStop()
+                Lifecycle.Event.ON_DESTROY -> onDestroy()
+                else -> {}
+            }
+        }
+
+
+        viewLifecycle.lifecycle.addObserver(lifecycleEventObserver)
+        onDispose{
+            viewLifecycle.lifecycle.removeObserver(lifecycleEventObserver)
         }
     }
 }
