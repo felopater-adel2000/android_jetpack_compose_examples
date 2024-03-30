fc6c8c31a23ed52384525a6e7e3a74f92afe42ed
felopater-adel2000 <felopater.adel@tera.ltd>
2024-03-30 17:19:10 +0200

    Handle Localization in Jetpack Compose 1. Change Layout Direction 2. Change Language

--- app/src/main/java/com/restart/jetpack_compose_examples/MainActivity.kt
+++ app/src/main/java/com/restart/jetpack_compose_examples/MainActivity.kt
@@ -1,169 +1,111 @@
 package com.restart.jetpack_compose_examples
 
 import android.os.Bundle
-import android.util.Log
-import android.view.WindowInsets.Side
-import android.widget.RemoteViews.RemoteView
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.DisposableEffect
-import androidx.compose.runtime.InternalComposeApi
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.SideEffect
-import androidx.compose.runtime.currentComposer
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.key
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.runtime.staticCompositionLocalOf
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.input.key.Key
-import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalLayoutDirection
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.LayoutDirection
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.compose.ui.window.Dialog
-import com.restart.jetpack_compose_examples.databinding.ActivityMainBinding
 import com.restart.jetpack_compose_examples.ui.theme.Jetpack_compose_examplesTheme
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
-import java.util.UUID
-import kotlin.math.log
+import java.util.Locale
 
+val LocalMainActivity = staticCompositionLocalOf<MainActivity> { noLocalProvidedFor("LocalActivity") }
+
+private fun noLocalProvidedFor(name: String): Nothing {
+    error("CompositionLocal $name not present")
+}
 class MainActivity : ComponentActivity() {
-    private lateinit var binding: ActivityMainBinding
-    override fun onCreate(savedInstanceState: Bundle?) {
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        binding.composeView.setContent {
 
-        }
+    private val localLayoutDirection = mutableStateOf(LayoutDirection.Ltr)
+    var currentLang = "en"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Jetpack_compose_examplesTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    TimerScreen()
+
+            CompositionLocalProvider(
+                LocalLayoutDirection provides localLayoutDirection.value,
+                LocalMainActivity provides this
+            ) {
+                Jetpack_compose_examplesTheme {
+                    // A surface container using the 'background' color from the theme
+                    Surface(
+                        modifier = Modifier.fillMaxSize(),
+                        color = MaterialTheme.colorScheme.background
+                    ) {
+                        TimerScreen()
+                    }
                 }
             }
         }
     }
-}
 
-const val TAG = "GlobalTag"
-
-
-@Composable
-fun Counter() {
-    // Define a state variable for the count
-    val count = remember { mutableStateOf(0) }
-
-    // Use SideEffect to log the current value of count
-    SideEffect {
-        // Called on every recomposition
-        Log.d(TAG, "Counter: Trigger SideEffect ${count.value}")
+    fun displayToast()
+    {
+        Toast.makeText(this, "Hello, World", Toast.LENGTH_LONG).show()
     }
 
-    Column {
-        Button(onClick = { count.value++ }) {
-            // This recomposition doesn't trigger the outer side effect
-            // every time button has been tapped
-            Text("Increase Count ${count.value}")
-        }
+    fun setCurrentLanguage(lang: String){
+        currentLang = lang
+        val config = resources.configuration
+        val locale = Locale(lang, if (lang == "en") "US" else "EG")
+        Locale.setDefault(locale)
+        config.setLocale(locale)
+        createConfigurationContext(config)
+        resources.updateConfiguration(config, resources.displayMetrics)
+        localLayoutDirection.value = if(lang == "en") LayoutDirection.Ltr else LayoutDirection.Rtl
     }
 }
 
-@Composable
-fun TestSideEffect()
-{
-    var count by remember { mutableStateOf(0) }
-
-    val some by remember { mutableStateOf(0) }
-    // this side effect not recomposed until the TestSideEffect recomposed
+private const val TAG = "MainActivity"
 
 
-    key(some){
-        SideEffect {
-            Log.d(TAG, "TestSideEffect: Trigger SideEffect")
-        }
-    }
-
-    Column(
-    ){
-
-        Button(onClick = { count++ }) {
-            Text(text = "count: ${count}")
-        }
-
-        Text(text = "Count: $count")
-
-    }
-
-}
-
-@Composable
-fun MyCounter()
-{
-    Column(
-        modifier = Modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ){
-        var count by remember { mutableStateOf(0) }
-
-        Button(onClick = { count++ }) {
-            Text(text = "Click me")
-        }
-
-        Text(text = "Count: $count")
-
-    }
-}
 
 
-suspend fun doSomething()
-{
-    delay(1000)
-    Log.d(TAG, "doSomething: Call")
-}
 
 @Preview(showSystemUi = true, device = "spec:width=1280dp,height=800dp,dpi=480")
 @Composable
 fun TimerScreen() {
-    val elapsedTime = remember { mutableStateOf(0) }
+    val name = "Felopater Adel"
 
-    DisposableEffect(Unit) {
-        val scope = CoroutineScope(Dispatchers.Default)
-        val job = scope.launch {
-            while (true) {
-                delay(1000)
-                elapsedTime.value += 1
-                Log.d(TAG, "TimerScreen: \"Timer is still working ${elapsedTime.value}\"")
-            }
-        }
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.Start
+    ) {
+        val activity = LocalMainActivity.current
+        Text(
+            text = stringResource(id = R.string.my_name, name),
+            modifier = Modifier.padding(16.dp),
+            fontSize = 24.sp
+        )
 
-        onDispose {
-            //job.cancel()
+        Spacer(modifier = Modifier.height(12.dp))
+
+        Button(onClick = { if(activity.currentLang == "en")activity.setCurrentLanguage("ar") else activity.setCurrentLanguage("en") }) {
+            Text(
+                text = stringResource(id = R.string.chnage_language)
+            )
         }
     }
-
-    Text(
-        text = "Elapsed Time: ${elapsedTime.value}",
-        modifier = Modifier.padding(16.dp),
-        fontSize = 24.sp
-    )
 }
