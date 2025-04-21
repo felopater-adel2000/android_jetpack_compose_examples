b02a4e8dcb9ea1c63177d63738f04ba0d8fd2c1c
felopater-adel2000 <felopater.adelboshra>
2025-04-21 19:39:37 +0200

    Add TestComposerScope and update MainActivity layout

--- app/src/main/java/com/restart/jetpack_compose_examples/MainActivity.kt
+++ app/src/main/java/com/restart/jetpack_compose_examples/MainActivity.kt
@@ -6,6 +6,7 @@ import android.view.WindowInsets.Side
 import android.widget.RemoteViews.RemoteView
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -18,12 +19,15 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.InternalComposeApi
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.NonRestartableComposable
+import androidx.compose.runtime.RecomposeScope
 import androidx.compose.runtime.SideEffect
 import androidx.compose.runtime.currentComposer
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.key
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -37,18 +41,21 @@ import com.restart.jetpack_compose_examples.databinding.ActivityMainBinding
 import com.restart.jetpack_compose_examples.ui.theme.Jetpack_compose_examplesTheme
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import java.util.UUID
+import kotlin.coroutines.Continuation
 import kotlin.math.log
 
 class MainActivity : ComponentActivity() {
     private lateinit var binding: ActivityMainBinding
+
+    var name = "Felopater Adel"
+
+    @OptIn(InternalComposeApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         binding = ActivityMainBinding.inflate(layoutInflater)
-        binding.composeView.setContent {
-
-        }
         super.onCreate(savedInstanceState)
         setContent {
             Jetpack_compose_examplesTheme {
@@ -57,11 +64,33 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-
+                    TestComposerScope("Felopater")
                 }
             }
         }
     }
+
+    @Composable
+    fun TestComposerScope(name: String) {
+
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+
+
+
+        }
+
+    }
 }
+
+fun String.doSomething(callback: (String) -> Unit) {
+    val result = "$this - done"
+    callback(result)
+}
+
+
 
 
