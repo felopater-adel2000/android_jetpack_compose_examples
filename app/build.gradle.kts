19a34c719bba3e3def1e514310840e8fb4176b7e
felopater-adel12 <felopater.sadek@maxab.io>
2025-08-03 12:15:47 +0300

    Update build.gradle.kts to reorganize test dependencies; remove duplicates and enhance clarity

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -72,7 +72,6 @@ dependencies {
     implementation("androidx.compose.material3:material3")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
-
     androidTestImplementation(platform("androidx.compose:compose-bom:2023.08.00"))
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
@@ -80,10 +79,6 @@ dependencies {
 
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-core:1.6.3")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.3")
-
-    // Compose UI Test
-    androidTestImplementation("androidx.compose.ui:ui-test-junit4")
-    debugImplementation("androidx.compose.ui:ui-test-manifest")
 
     //Navigation Component
     val nav_version = "2.9.3"
@@ -97,6 +92,21 @@ dependencies {
     implementation("io.insert-koin:koin-androidx-compose:4.1.0")
     implementation("io.insert-koin:koin-androidx-navigation:4.1.0")
 
+
+    /** Testing **/
+
+    // Core & JUnit
+    androidTestImplementation("androidx.test:runner:1.5.2")
+    androidTestImplementation("androidx.test:rules:1.5.0")
+    testImplementation("androidx.test:core:1.7.0")
+    androidTestImplementation("androidx.test:core:1.7.0")
+    androidTestImplementation("androidx.arch.core:core-testing:2.2.0")
+
+
+    // Compose UI Test
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4")
+    debugImplementation("androidx.compose.ui:ui-test-manifest")
+
     // Mockk
     val mockkVersion = "1.14.5"
     testImplementation("io.mockk:mockk-android:${mockkVersion}")
@@ -104,26 +114,18 @@ dependencies {
     androidTestImplementation("io.mockk:mockk-android:${mockkVersion}")
     androidTestImplementation("io.mockk:mockk-agent:${mockkVersion}")
 
-    // Espresso
-    androidTestImplementation("androidx.test:runner:1.5.2")
-    androidTestImplementation("androidx.test:rules:1.5.0")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.7.0")
-    androidTestImplementation("androidx.test.espresso:espresso-contrib:3.7.0")
-    androidTestImplementation("androidx.test.espresso:espresso-intents:3.7.0")
-    androidTestImplementation("com.android.support.test.espresso:espresso-contrib:3.0.2")
+    // Test Navigation
+    androidTestImplementation("androidx.navigation:navigation-testing:2.9.3")
 
-    androidTestImplementation("androidx.arch.core:core-testing:2.2.0")
-
-
+    // Test Fragment
     val fragment_version = "1.8.8"
     debugImplementation("androidx.fragment:fragment-testing-manifest:$fragment_version")
     androidTestImplementation("androidx.fragment:fragment-testing:$fragment_version")
 
-    androidTestImplementation("io.insert-koin:koin-test:2.2.3")
-    testImplementation("io.insert-koin:koin-test:2.2.3")
+    // Test Koin
+    androidTestImplementation("io.insert-koin:koin-test:4.1.0")
+    testImplementation("io.insert-koin:koin-test:4.1.0")
+    testImplementation("io.insert-koin:koin-test-junit4:4.1.0")
+    androidTestImplementation("io.insert-koin:koin-test-junit4:4.1.0")
 
-    // Test Naviagetion
-    androidTestImplementation("androidx.navigation:navigation-testing:2.9.3")
-
-    implementation("androidx.test:core:1.7.0")
 }
