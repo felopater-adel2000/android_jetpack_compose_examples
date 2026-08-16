96690b37dc46f85f62231aebc3e400d3dfe9ba1e
felopater-adel12 <felopater.sadek@maxab.io>
2026-08-16 20:42:24 +0300

    Update Gradle to 9.1.0 and remove unused Compose UI code in MainActivity

--- app/src/main/java/com/restart/jetpack_compose_examples/MainActivity.kt
+++ app/src/main/java/com/restart/jetpack_compose_examples/MainActivity.kt
@@ -3,15 +3,6 @@ package com.restart.jetpack_compose_examples
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import com.maxab.theme.MarketPlaceSemantic
 import com.restart.jetpack_compose_examples.databinding.ActivityMainBinding
 import com.restart.jetpack_compose_examples.ui.theme.Jetpack_compose_examplesTheme
 
@@ -25,16 +16,6 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             Jetpack_compose_examplesTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Text(
-                        text = "Hello World", fontSize = 30.sp, modifier = Modifier.padding(50.dp),
-                        color = MarketPlaceSemantic.semanticMarketplaceColorBackgroundAccentBlueDefault
-                    )
-                }
             }
         }
     }
